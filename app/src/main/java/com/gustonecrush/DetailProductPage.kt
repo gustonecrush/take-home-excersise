96e0c0af6234bfb30782140b2cfba8386eabd530
gustonecrush <codeboxezz@gmail.com>
2022-12-10 22:43:55 +0700

    built detail product page

---
+++ app/src/main/java/com/gustonecrush/DetailProductPage.kt
@@ -0,0 +1,11 @@
+package com.gustonecrush
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProductPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product_page)
+    }
+}
